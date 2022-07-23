3af501c4630cccdd3ca9fb638d69574eca696603
Hoodoo42 <create@hoodooverse.ca>
2022-07-23 16:10:54 -0600

    spent some time troubleshooting why my opponents health wasnt being stored with the new value, my main issue being I had a typo. moving on to haveing the display refresh every time a health point is changed in real time

--- js/app.js
+++ js/app.js
@@ -5,13 +5,24 @@
 function chooseMagikarp(details) {
   let magikarp = {
     name: `Magikarp`,
-    imgURL: `images/magikarp.webp`,
+    imgURL: `/images/magikarp.webp`,
     health: 50,
-    attack: [`splash`, `struggle`, `frustration`],
+    attack1: `Splash`,
+    attack2: `Struggle`,
+    attack3:`Frusteration`
   };
 
   let magikarpJson = JSON.stringify(magikarp);
   Cookies.set(`chosenPokemon`, magikarpJson);
+
+  Cookies.set(`playerHealth`, magikarp[`health`])
+  
+
+  let gengarJson = JSON.stringify(gengar)
+  Cookies.set(`compSelection`, gengarJson)
+
+  Cookies.set(`opponentHealth`,gengar[`health`] )
+
   location.href = `battle.html`;
 }
 
@@ -20,20 +31,24 @@ function chooseMew(details) {
     name: `Mew`,
     imgURL: `images/mew.webp`,
     health: 50,
-    attack: [`splash`, `struggle`, `frustration`],
+    attack1: `Focus Blast`,
+    attack2: `Solar Beam`,
+    attack3: `Phycic`
   };
 
   let mewJson = JSON.stringify(mew);
-  Cookies.set(`chosePokemon`, mewJson);
+  Cookies.set(`chosenPokemon`, mewJson);
   location.href = `battle.html`;
 }
 
 function chooseFrillish(details) {
   let frillish = {
     name: `Frillish`,
-    imgURL: `images/frillish.webp`,
+    imgURL: `/images/frillish.webp`,
     health: 50,
-    attack: [`splash`, `struggle`, `frustration`],
+    attack1: `Water Absorb`,
+    attack2: `Cursed Body`,
+    attack3: `Poison Sting`,
   };
 
   let frillishJson = JSON.stringify(frillish);
@@ -41,6 +56,15 @@ function chooseFrillish(details) {
   location.href = `battle.html`;
 }
 
+
+let gengar ={
+    name: `Gengar`,
+    imgURL: `images/bad.webp`, 
+    health: 50,
+    attack1: `Focus Blast`,
+    attack2: `Shadow Punch`,
+    attack3: `Sludge Wave`
+};
 
 // these select an element by the id, and add the eventListener that will set the functions off
 let fish = document.getElementById(`magikarp_button`);
--- js/battle.js
+++ js/battle.js
@@ -4,6 +4,105 @@
 let getPokemon = Cookies.get(`chosenPokemon`);
 let iChooseYou = JSON.parse(getPokemon);
 
+let getOpponent = Cookies.get(`compSelection`);
+let opponent = JSON.parse(getOpponent);
+
+let opponentHealth = Cookies.get(`opponentHealth`);
+let yourHealth = Cookies.get(`playerHealth`)
 // setting up the conditionals on which pokemon(object) will be diplayed on the page and how.
+
 if (iChooseYou[`name`] === `Magikarp`) {
+  // let battleArea = document.getElementById(`battle_grounds`);
+  // battleArea[`innerHTML`] =
+  document.body.insertAdjacentHTML(
+    `afterbegin`,
+
+    `
+        <section id="player_side">
+           <img src ="${iChooseYou[`imgURL`]}">
+           <h3 id="your_hp_display"> ${yourHealth}</h3>
+           <button id="attack_button_one"> ${iChooseYou[`attack1`]} </button>
+           <button id="attack_button_two"> ${iChooseYou[`attack2`]} </button>
+           <button id="attack_button_three"> ${iChooseYou[`attack3`]} </button>
+         </section>  
+         
+      `
+  );
+  document.body.insertAdjacentHTML(
+    `afterbegin`,
+
+    `<section id="opponent_side">
+        <img src ="${opponent[`imgURL`]}">
+        <h3 id="opponent_hp_display"> ${opponentHealth}</h3>
+       
+        </section>
+        `
+  );
+
+  function mainAttack(details) {
+    let attackPoints = opponentHealth - 2;
+    Cookies.set(`opponentHealth`, attackPoints);
+ 
+    let opponentHp = document.getElementById(`opponent_hp_display`);
+    opponentHp[`innerHTML`] = attackPoints
+
 }
+
+
+
+
+
+
+
+
+
+
+} else if (iChooseYou[`name`] === `Mew`) {
+  document.body.insertAdjacentHTML(
+    `afterbegin`,
+    `
+           <img src ="${iChooseYou[`imgURL`]}">
+           <h3> ${iChooseYou[`health`]}</h3>
+           <button> ${iChooseYou[`attack1`]} </button>
+           <button> ${iChooseYou[`attack2`]} </button>
+           <button> ${iChooseYou[`attack3`]} </button>
+         
+      `
+  );
+  document.body.insertAdjacentHTML(
+    `afterbegin`,
+
+    `<section id="opponent_side">
+        <img src ="${opponent[`imgURL`]}">
+        <h3> ${opponent[`health`]}</h3>
+       
+        </section>
+        `
+  );
+} else if (iChooseYou[`name`] === `Frillish`) {
+  document.body.insertAdjacentHTML(
+    `afterbegin`,
+    `
+               <img src ="${iChooseYou[`imgURL`]}">
+               <h3> ${iChooseYou[`health`]}</h3>
+               <button> ${iChooseYou[`attack1`]} </button>
+               <button> ${iChooseYou[`attack2`]} </button>
+               <button> ${iChooseYou[`attack3`]} </button>
+             
+          `
+  );
+
+  document.body.insertAdjacentHTML(
+    `afterbegin`,
+
+    `<section id="opponent_side">
+            <img src ="${opponent[`imgURL`]}">
+            <h3> ${opponent[`health`]}</h3>
+           
+            </section>
+            `
+  );
+}
+
+let attackOne = document.getElementById(`attack_button_one`);
+attackOne.addEventListener(`click`, mainAttack);
