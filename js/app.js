9dc2f45f44318f4a77488eb6e591348c76ad5920
Hoodoo42 <create@hoodooverse.ca>
2022-07-25 23:43:47 -0600

    I fixed my issue where the display health kept refreshing everytime i refreshed the page, but the cookies remained stored. I then worked towards making each button for each pokemon work. I would like to come back and figure out a better way to end the match, also figure out why my disabled buttons suddenly stopped working. I would also like to make my h pvalues random, and then add a heal button. ALSO I want to add some bad guys and randomize them

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// each function will create an object with onfo on the selected pokemon. 
+// each function will create an object with onfo on the selected pokemon.
 // it will then serialize the object
 // then it will save the serialized object in a cookie
 
@@ -9,19 +9,16 @@ function chooseMagikarp(details) {
     health: 50,
     attack1: `Splash`,
     attack2: `Struggle`,
-    attack3:`Frusteration`
+    attack3: `Frusteration`,
   };
 
   let magikarpJson = JSON.stringify(magikarp);
   Cookies.set(`chosenPokemon`, magikarpJson);
+  Cookies.set(`playerHealth`, magikarp[`health`]);
 
-  Cookies.set(`playerHealth`, magikarp[`health`])
-  
-
-  let gengarJson = JSON.stringify(gengar)
-  Cookies.set(`compSelection`, gengarJson)
-
-  Cookies.set(`opponentHealth`,gengar[`health`] )
+  let gengarJson = JSON.stringify(gengar);
+  Cookies.set(`compSelection`, gengarJson);
+  Cookies.set(`opponentHealth`, gengar[`health`]);
 
   location.href = `battle.html`;
 }
@@ -33,11 +30,17 @@ function chooseMew(details) {
     health: 50,
     attack1: `Focus Blast`,
     attack2: `Solar Beam`,
-    attack3: `Phycic`
+    attack3: `Phycic`,
   };
 
   let mewJson = JSON.stringify(mew);
   Cookies.set(`chosenPokemon`, mewJson);
+  Cookies.set(`playerHealth`, mew[`health`]);
+
+  let gengarJson = JSON.stringify(gengar);
+  Cookies.set(`compSelection`, gengarJson);
+  Cookies.set(`opponentHealth`, gengar[`health`]);
+
   location.href = `battle.html`;
 }
 
@@ -53,17 +56,22 @@ function chooseFrillish(details) {
 
   let frillishJson = JSON.stringify(frillish);
   Cookies.set(`chosenPokemon`, frillishJson);
+  Cookies.set(`playerHealth`, frillish[`health`]);
+
+  let gengarJson = JSON.stringify(gengar);
+  Cookies.set(`compSelection`, gengarJson);
+  Cookies.set(`opponentHealth`, gengar[`health`]);
+
   location.href = `battle.html`;
 }
 
-
-let gengar ={
-    name: `Gengar`,
-    imgURL: `images/bad.webp`, 
-    health: 50,
-    attack1: `Focus Blast`,
-    attack2: `Shadow Punch`,
-    attack3: `Sludge Wave`
+let gengar = {
+  name: `Gengar`,
+  imgURL: `images/bad.webp`,
+  health: 50,
+  attack1: `Focus Blast`,
+  attack2: `Shadow Punch`,
+  attack3: `Sludge Wave`,
 };
 
 // these select an element by the id, and add the eventListener that will set the functions off
--- js/battle.js
+++ js/battle.js
@@ -1,4 +1,4 @@
-// setting a variable that will get the serialized object that was selected
+// setting a variable that will get the serialized object(pokemon description) that was selected
 // set a variable that will hold the de-serialized object
 
 let getPokemon = Cookies.get(`chosenPokemon`);
@@ -7,13 +7,12 @@ let iChooseYou = JSON.parse(getPokemon);
 let getOpponent = Cookies.get(`compSelection`);
 let opponent = JSON.parse(getOpponent);
 
-let opponentHealth = Cookies.get(`opponentHealth`);
-let yourHealth = Cookies.get(`playerHealth`)
+// these variable hold specifically the health values
+let theOpponentHealth = Cookies.get(`opponentHealth`);
+let yourHealth = Cookies.get(`playerHealth`);
 // setting up the conditionals on which pokemon(object) will be diplayed on the page and how.
 
 if (iChooseYou[`name`] === `Magikarp`) {
-  // let battleArea = document.getElementById(`battle_grounds`);
-  // battleArea[`innerHTML`] =
   document.body.insertAdjacentHTML(
     `afterbegin`,
 
@@ -21,9 +20,15 @@ if (iChooseYou[`name`] === `Magikarp`) {
         <section id="player_side">
            <img src ="${iChooseYou[`imgURL`]}">
            <h3 id="your_hp_display"> ${yourHealth}</h3>
-           <button id="attack_button_one"> ${iChooseYou[`attack1`]} </button>
-           <button id="attack_button_two"> ${iChooseYou[`attack2`]} </button>
-           <button id="attack_button_three"> ${iChooseYou[`attack3`]} </button>
+           <button id="attack_button_one" class="disable" > ${
+             iChooseYou[`attack1`]
+           } </button>
+           <button id="attack_button_two" class="disable"> ${
+             iChooseYou[`attack2`]
+           } </button>
+           <button id="attack_button_three" class="disable"> ${
+             iChooseYou[`attack3`]
+           } </button>
          </section>  
          
       `
@@ -33,40 +38,186 @@ if (iChooseYou[`name`] === `Magikarp`) {
 
     `<section id="opponent_side">
         <img src ="${opponent[`imgURL`]}">
-        <h3 id="opponent_hp_display"> ${opponentHealth}</h3>
+        <h3 id="opponent_hp_display"> ${theOpponentHealth}</h3>
        
         </section>
         `
   );
+  // these functions will determin battle play. how much hp each attack will take, whats happens when someone get to 0.
 
   function mainAttack(details) {
-    let attackPoints = opponentHealth - 2;
-    Cookies.set(`opponentHealth`, attackPoints);
- 
+    theOpponentHealth = theOpponentHealth - 2;
+    Cookies.set(`opponentHealth`, theOpponentHealth);
+
     let opponentHp = document.getElementById(`opponent_hp_display`);
-    opponentHp[`innerHTML`] = attackPoints
+    opponentHp[`innerHTML`] = theOpponentHealth;
+    // these if statements set the conditionals to a 0hp will end the match decalring who won and where to go next.
 
-}
+    if (theOpponentHealth <= 0) {
+      document.body.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <section>
+        <h2> You win! </h2>
+        <button id="next_match"> Next Match</button>
+       
+        `
+      );
+      document.getElementsByClassName(`.disable`).disabled = true;
+      function takeHome(details) {
+        location.href = `index.html`;
+        let nextMatch = document.getElementById(`next_match`);
+        nextMatch.addEventListener(`click`, takeHome);
+      }
+    }
+    // this renders a new hp value and saves it in the cookie
+    yourHealth = yourHealth - 3;
+    Cookies.set(`playerHealth`, yourHealth);
+    // this takes the new hp and realtime displays it in the browser
+    let yourHp = document.getElementById(`your_hp_display`);
+    yourHp[`innerHTML`] = yourHealth;
 
+    if (yourHealth <= 0) {
+      document.body.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <section>
+        <h2> You lose! ${opponent[`name`]} got away! </h2>
+        <button id="next_match"> Next Match</button>
+        </section>
+        `
+      );
 
+      //   well this disable feature worked like a charm, until it suddenly didnt. it greyd out every attack button, and under that
+      // was this lovely button that takes you home.  I will have to come back and troubleshoot.
+      document.getElementsByClassName(`.disable`).disabled = true;
+      // I wasnt entirely sure how to "end the match" so I just improvised with a home button. Though I am sure there is a way to
+      // stop the gameplay capabitlites and refresh
+      function takeHome(details) {
+        location.href = `index.html`;
+      }
+      let nextMatch = document.getElementById(`next_match`);
+      nextMatch.addEventListener(`click`, takeHome);
+    }
+  }
 
+  function secondAttack(details) {
+    theOpponentHealth = theOpponentHealth - 4;
+    Cookies.set(`opponentHealth`, theOpponentHealth);
 
+    let opponentHp = document.getElementById(`opponent_hp_display`);
+    opponentHp[`innerHTML`] = theOpponentHealth;
 
+    if (theOpponentHealth <= 0) {
+      document.body.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <section>
+        <h2> You win! </h2>
+        <button id="next_match"> Next Match</button>
+       
+        `
+      );
+      function takeHome(details) {
+        location.href = `index.html`;
+      }
+      let nextMatch = document.getElementById(`next_match`);
+      nextMatch.addEventListener(`click`, takeHome);
+      document.getElementsByClassName(`disable`).disabled = true;
+    }
 
+    yourHealth = yourHealth - 3;
+    Cookies.set(`playerHealth`, yourHealth);
 
+    let yourHp = document.getElementById(`your_hp_display`);
+    yourHp[`innerHTML`] = yourHealth;
 
+    if (yourHealth <= 0) {
+      document.body.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <section>
+        <h2> You lose! ${opponent[`name`]} got away! </h2>
+        <button id="next_match"> Next Match</button>
+        </section>
+        `
+      );
 
+      function takeHome(details) {
+        location.href = `index.html`;
+      }
+      let nextMatch = document.getElementById(`next_match`);
+      nextMatch.addEventListener(`click`, takeHome);
+      document.getElementsByClassName(`disable`).disabled = true;
+    }
+  }
 
+  function finalAttack(details) {
+    theOpponentHealth = theOpponentHealth - 5;
+    Cookies.set(`opponentHealth`, theOpponentHealth);
+
+    let opponentHp = document.getElementById(`opponent_hp_display`);
+    opponentHp[`innerHTML`] = theOpponentHealth;
+
+    if (theOpponentHealth <= 0) {
+      document.body.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <section>
+        <h2> You win! </h2>
+        <button id="next_match"> Next Match</button>
+       
+        `
+      );
+      function takeHome(details) {
+        location.href = `index.html`;
+      }
+      let nextMatch = document.getElementById(`next_match`);
+      nextMatch.addEventListener(`click`, takeHome);
+      document.getElementsByClassName(`disable`).disabled = true;
+    }
+
+    yourHealth = yourHealth - 3;
+    Cookies.set(`playerHealth`, yourHealth);
+
+    let yourHp = document.getElementById(`your_hp_display`);
+    yourHp[`innerHTML`] = yourHealth;
+
+    if (yourHealth <= 0) {
+      document.body.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <section>
+        <h2> You lose! ${opponent[`name`]} got away! </h2>
+        <button id="next_match"> Next Match</button>
+        </section>
+        `
+      );
+
+      function takeHome(details) {
+        location.href = `index.html`;
+      }
+      let nextMatch = document.getElementById(`next_match`);
+      nextMatch.addEventListener(`click`, takeHome);
+      document.getElementsByClassName(`disable`).disabled = true;
+    }
+  }
 } else if (iChooseYou[`name`] === `Mew`) {
   document.body.insertAdjacentHTML(
     `afterbegin`,
-    `
+    `<section id="player_side">
            <img src ="${iChooseYou[`imgURL`]}">
-           <h3> ${iChooseYou[`health`]}</h3>
-           <button> ${iChooseYou[`attack1`]} </button>
-           <button> ${iChooseYou[`attack2`]} </button>
-           <button> ${iChooseYou[`attack3`]} </button>
-         
+           <h3 id="your_hp_display"> ${iChooseYou[`health`]}</h3>
+           <button id="attack_button_one" class="disable"> ${
+             iChooseYou[`attack1`]
+           } </button>
+           <button id="attack_button_two" class="disable"> ${
+             iChooseYou[`attack2`]
+           } </button>
+           <button id="attack_button_three" class="disable"> ${
+             iChooseYou[`attack3`]
+           } </button>
+           </section> 
       `
   );
   document.body.insertAdjacentHTML(
@@ -74,21 +225,178 @@ if (iChooseYou[`name`] === `Magikarp`) {
 
     `<section id="opponent_side">
         <img src ="${opponent[`imgURL`]}">
-        <h3> ${opponent[`health`]}</h3>
+        <h3 id="opponent_hp_display"> ${theOpponentHealth}</h3>
        
         </section>
         `
   );
+
+  function mainAttack(details) {
+    theOpponentHealth = theOpponentHealth - 2;
+    Cookies.set(`opponentHealth`, theOpponentHealth);
+
+    let opponentHp = document.getElementById(`opponent_hp_display`);
+    opponentHp[`innerHTML`] = theOpponentHealth;
+
+    if (theOpponentHealth <= 0) {
+      document.body.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <section>
+        <h2> You win! </h2>
+        <button id="next_match"> Next Match</button>
+        `
+      );
+      function takeHome(details) {
+        location.href = `index.html`;
+      }
+      let nextMatch = document.getElementById(`next_match`);
+      nextMatch.addEventListener(`click`, takeHome);
+      document.getElementsByClassName(`disable`).disabled = true;
+    }
+
+    yourHealth = yourHealth - 1;
+    Cookies.set(`playerHealth`, yourHealth);
+
+    let yourHp = document.getElementById(`your_hp_display`);
+    yourHp[`innerHTML`] = yourHealth;
+
+    if (yourHealth <= 0) {
+      document.body.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <section>
+        <h2> You lose! ${opponent[`name`]} got away! </h2>
+        <button id="next_match"> Next Match</button>
+        </section>
+        `
+      );
+      function takeHome(details) {
+        location.href = `index.html`;
+      }
+      let nextMatch = document.getElementById(`next_match`);
+      nextMatch.addEventListener(`click`, takeHome);
+      document.getElementsByClassName(`disable`).disabled = true;
+    }
+  }
+
+  function secondAttack(details) {
+    theOpponentHealth = theOpponentHealth - 4;
+    Cookies.set(`opponentHealth`, theOpponentHealth);
+
+    let opponentHp = document.getElementById(`opponent_hp_display`);
+    opponentHp[`innerHTML`] = theOpponentHealth;
+
+    if (theOpponentHealth <= 0) {
+      document.body.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <section>
+        <h2> You win! </h2>
+        <button id="next_match"> Next Match</button>
+       
+        `
+      );
+      function takeHome(details) {
+        location.href = `index.html`;
+      }
+      let nextMatch = document.getElementById(`next_match`);
+      nextMatch.addEventListener(`click`, takeHome);
+      document.getElementsByClassName(`disable`).disabled = true;
+    }
+
+    yourHealth = yourHealth - 3;
+    Cookies.set(`playerHealth`, yourHealth);
+
+    let yourHp = document.getElementById(`your_hp_display`);
+    yourHp[`innerHTML`] = yourHealth;
+
+    if (yourHealth <= 0) {
+      document.body.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <section>
+        <h2> You lose! ${opponent[`name`]} got away! </h2>
+        <button id="next_match"> Next Match</button>
+        </section>
+        `
+      );
+
+      function takeHome(details) {
+        location.href = `index.html`;
+      }
+      let nextMatch = document.getElementById(`next_match`);
+      nextMatch.addEventListener(`click`, takeHome);
+      document.getElementsByClassName(`disable`).disabled = true;
+    }
+  }
+
+  function secondAttack(details) {
+    theOpponentHealth = theOpponentHealth - 4;
+    Cookies.set(`opponentHealth`, theOpponentHealth);
+
+    let opponentHp = document.getElementById(`opponent_hp_display`);
+    opponentHp[`innerHTML`] = theOpponentHealth;
+
+    if (theOpponentHealth <= 0) {
+      document.body.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <section>
+        <h2> You win! </h2>
+        <button id="next_match"> Next Match</button>
+       
+        `
+      );
+      function takeHome(details) {
+        location.href = `index.html`;
+      }
+      let nextMatch = document.getElementById(`next_match`);
+      nextMatch.addEventListener(`click`, takeHome);
+      document.getElementsByClassName(`disable`).disabled = true;
+    }
+
+    yourHealth = yourHealth - 3;
+    Cookies.set(`playerHealth`, yourHealth);
+
+    let yourHp = document.getElementById(`your_hp_display`);
+    yourHp[`innerHTML`] = yourHealth;
+
+    if (yourHealth <= 0) {
+      document.body.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <section>
+        <h2> You lose! ${opponent[`name`]} got away! </h2>
+        <button id="next_match"> Next Match</button>
+        </section>
+        `
+      );
+
+      function takeHome(details) {
+        location.href = `index.html`;
+      }
+      let nextMatch = document.getElementById(`next_match`);
+      nextMatch.addEventListener(`click`, takeHome);
+      document.getElementsByClassName(`disable`).disabled = true;
+    }
+  }
 } else if (iChooseYou[`name`] === `Frillish`) {
   document.body.insertAdjacentHTML(
     `afterbegin`,
-    `
+    `<section id="player_side">
                <img src ="${iChooseYou[`imgURL`]}">
-               <h3> ${iChooseYou[`health`]}</h3>
-               <button> ${iChooseYou[`attack1`]} </button>
-               <button> ${iChooseYou[`attack2`]} </button>
-               <button> ${iChooseYou[`attack3`]} </button>
-             
+               <h3 id="your_hp_display"> ${iChooseYou[`health`]}</h3>
+               <button id="attack_button_one" class="disable"> ${
+                 iChooseYou[`attack1`]
+               } </button>
+               <button id="attack_button_two" class="disable"> ${
+                 iChooseYou[`attack2`]
+               } </button>
+               <button id="attack_button_three" class="disable"> ${
+                 iChooseYou[`attack3`]
+               } </button>
+               </section> 
           `
   );
 
@@ -97,12 +405,170 @@ if (iChooseYou[`name`] === `Magikarp`) {
 
     `<section id="opponent_side">
             <img src ="${opponent[`imgURL`]}">
-            <h3> ${opponent[`health`]}</h3>
+            <h3 id="opponent_hp_display"> ${theOpponentHealth}</h3>
            
             </section>
             `
   );
+
+  function mainAttack(details) {
+    theOpponentHealth = theOpponentHealth - 2;
+    Cookies.set(`opponentHealth`, theOpponentHealth);
+
+    let opponentHp = document.getElementById(`opponent_hp_display`);
+    opponentHp[`innerHTML`] = theOpponentHealth;
+
+    if (theOpponentHealth <= 0) {
+      document.body.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <section>
+        <h2> You win! </h2>
+        <button id="next_match"> Next Match</button>
+        </section>
+        `
+      );
+      function takeHome(details) {
+        location.href = `index.html`;
+      }
+      let nextMatch = document.getElementById(`next_match`);
+      nextMatch.addEventListener(`click`, takeHome);
+      document.getElementsByClassName(`disable`).disabled = true;
+    }
+
+    yourHealth = yourHealth - 1;
+    Cookies.set(`playerHealth`, yourHealth);
+
+    let yourHp = document.getElementById(`your_hp_display`);
+    yourHp[`innerHTML`] = yourHealth;
+
+    if (yourHealth <= 0) {
+      document.body.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <section>
+        <h2> You lose! ${opponent[`name`]} got away! </h2>
+        <button id="next_match"> Next Match</button>
+        </section>
+        `
+      );
+      function takeHome(details) {
+        location.href = `index.html`;
+      }
+      let nextMatch = document.getElementById(`next_match`);
+      nextMatch.addEventListener(`click`, takeHome);
+      document.getElementsByClassName(`disable`).disabled = true;
+    }
+  }
+  function secondAttack(details) {
+    theOpponentHealth = theOpponentHealth - 4;
+    Cookies.set(`opponentHealth`, theOpponentHealth);
+
+    let opponentHp = document.getElementById(`opponent_hp_display`);
+    opponentHp[`innerHTML`] = theOpponentHealth;
+
+    if (theOpponentHealth <= 0) {
+      document.body.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <section>
+        <h2> You win! </h2>
+        <button id="next_match"> Next Match</button>
+       
+        `
+      );
+      function takeHome(details) {
+        location.href = `index.html`;
+      }
+      let nextMatch = document.getElementById(`next_match`);
+      nextMatch.addEventListener(`click`, takeHome);
+      document.getElementsByClassName(`disable`).disabled = true;
+    }
+
+    yourHealth = yourHealth - 3;
+    Cookies.set(`playerHealth`, yourHealth);
+
+    let yourHp = document.getElementById(`your_hp_display`);
+    yourHp[`innerHTML`] = yourHealth;
+
+    if (yourHealth <= 0) {
+      document.body.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <section>
+        <h2> You lose! ${opponent[`name`]} got away! </h2>
+        <button id="next_match"> Next Match</button>
+        </section>
+        `
+      );
+
+      function takeHome(details) {
+        location.href = `index.html`;
+      }
+      let nextMatch = document.getElementById(`next_match`);
+      nextMatch.addEventListener(`click`, takeHome);
+      document.getElementsByClassName(`disable`).disabled = true;
+    }
+  }
+
+  function secondAttack(details) {
+    theOpponentHealth = theOpponentHealth - 4;
+    Cookies.set(`opponentHealth`, theOpponentHealth);
+
+    let opponentHp = document.getElementById(`opponent_hp_display`);
+    opponentHp[`innerHTML`] = theOpponentHealth;
+
+    if (theOpponentHealth <= 0) {
+      document.body.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <section>
+        <h2> You win! </h2>
+        <button id="next_match"> Next Match</button>
+       
+        `
+      );
+      function takeHome(details) {
+        location.href = `index.html`;
+      }
+      let nextMatch = document.getElementById(`next_match`);
+      nextMatch.addEventListener(`click`, takeHome);
+      document.getElementsByClassName(`disable`).disabled = true;
+    }
+
+    yourHealth = yourHealth - 3;
+    Cookies.set(`playerHealth`, yourHealth);
+
+    let yourHp = document.getElementById(`your_hp_display`);
+    yourHp[`innerHTML`] = yourHealth;
+
+    if (yourHealth <= 0) {
+      document.body.insertAdjacentHTML(
+        `beforeend`,
+        `
+        <section>
+        <h2> You lose! ${opponent[`name`]} got away! </h2>
+        <button id="next_match"> Next Match</button>
+        </section>
+        `
+      );
+
+      function takeHome(details) {
+        location.href = `index.html`;
+      }
+      let nextMatch = document.getElementById(`next_match`);
+      nextMatch.addEventListener(`click`, takeHome);
+      document.getElementsByClassName(`disable`).disabled = true;
+    }
+  }
 }
 
+// setting a variable to access the button with its id, then attaching an eventListener to "attack" when clicked
 let attackOne = document.getElementById(`attack_button_one`);
 attackOne.addEventListener(`click`, mainAttack);
+
+let attackTwo = document.getElementById(`attack_button_two`);
+attackTwo.addEventListener(`click`, secondAttack);
+
+let attackThree = document.getElementById(`attack_button_three`);
+attackThree.addEventListener(`click`, finalAttack);
